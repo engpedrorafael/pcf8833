dffd1f5ed82f526306559993a2b305ab2572a020
casa <casa@NB300.(none)>
2014-02-13 17:51:54 +0000

    Added example

---
+++ examples/loadImage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from pcf8833 import lcd
+
+#Load the LCD driver
+myLcd = lcd.Driver()
+
+#Load teh image to the LCD
+myLCD.loadImage("./images/testImg.jpg")
+
+print "Done. You should see now a test image in the LCD."
+
